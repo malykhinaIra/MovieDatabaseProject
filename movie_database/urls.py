b62b959763df14260cff12bc7fb7285e42cf16bf
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2023-05-25 14:17:51 +0300

    final

--- movie_database/urls.py
+++ movie_database/urls.py
@@ -1,17 +1,4 @@
 """movie_database URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -24,13 +11,13 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
                   path('', main_views.index, name='index'),
-                  path('signup', user_views.SignupView.signup, name='signup'),
+                  path('signup', user_views.signup, name='signup'),
                   path('search/', catalog_views.search, name='search'),
                   path('admin/', admin.site.urls),
                   path('user/', include('users.urls')),
                   path('movie/', include('catalog.urls')),
-                  path('login', user_views.LoginUserView.login_user, name='login_user'),
-                  path('change_password', user_views.ChangePasswordView.change_password, name='change_password'),
+                  path('login', user_views.LoginUser, name='login_user'),
+                  path('change_password', user_views.change_password, name='change_password'),
                   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
