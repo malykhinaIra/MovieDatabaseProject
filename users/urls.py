2986ba53ffc8393f0eaecf49e990880a99b806ac
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2023-05-25 13:57:51 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('<str:username>/', views.user_profile, name = 'user_profile'),
+    path('<str:username>/', views.UserProfileView.user_profile, name = 'user_profile'),
 ]
 
 
